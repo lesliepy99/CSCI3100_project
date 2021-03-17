0773eb5d7595cd6c2fa2ebe8a47dfdab9ef228f2
lesliepy99 <1155124579@link.cuhk.edu.hk>
2021-03-17 22:57:23 +0800

    Define data models

--- database.js
+++ database.js
@@ -2,53 +2,103 @@ const mongoose = require('mongoose');
 var dbUri = "mongodb+srv://csci3100_project_team:csci3100project@cluster0.muf9n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 var Schema = mongoose.Schema;
-mongoose.connect(dbUri,{
-    useNewUrlParser:true,
+mongoose.connect(dbUri, {
+    useNewUrlParser: true,
     useUnifiedTopology: true
 });
 var db = mongoose.connection;
 
-db.on('error',console.error.bind(console,'connection:error:'));
+db.on('error', console.error.bind(console, 'connection:error:'));
 
-db.once('open',function(){
+db.once('open', function () {
     console.log("Mongodb connected!");
 })
 
-var UserSchama = Schema({
-    name:{type:String, required:true},
-    password:{type:String, required:true},
-    email:{type:String,required:true,unique:true},
-    school:{type:String,required:true},
-    year:{type:Number},
-    ranki:{type:String},
-    location:{type:String},
-    chat_list:[{
-        userId:{type:Schema.ObjectId},
-        last_chat_time:{type:Date},
-        most_recent_message:{type:String},
-        unread_count:{type:Number}
-        }]
+var UserSchema = Schema({
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    school: { type: String, required: true },
+    year: { type: Number },
+    ranki: { type: String },
+    location: { type: String },
+    chat_list: [{
+        userId: { type: Schema.ObjectId },
+        last_chat_time: { type: Date },
+        most_recent_message: { type: String },
+        unread_count: { type: Number }
+    }]
 });
 
-UserModel = mongoose.model('User',UserSchama);
+var GoodSchema = Schema({
+    name: { type: String, required: true },
+    userId: { type: Schema.ObjectId, required: true },
+    isSold: { type: Boolean, default: 0 },
+    tags: [{
+        tag: { type: String }
 
-createUser = (name,password,email,school)=> new Promise((resolve,reject)=>{
-    UserModel.findOne({email:email},(err,user)=>{
-        if(err) reject(err);
-        else if(!user){
-            UserModel.create({name:name,password:password,email:email,school:school},(err,result)=>{
-               if (err || !result) reject(err);
-               else {
-                   resolve(true);
-               }
+    }],
+    number_of_views: { type: Number, default: 0 },
+    number_of_likes: { type: Number, default: 0 },
+    good_image: [{
+        imageUrl: { type: String }
+    }],
+    description: { type: String },
+    estimated_price: { type: Number }
+
+});
+
+var ChatSchema = Schema({
+    two_user_id: [{
+        id: { type: Schema.ObjectId }
+    }],
+    messages: [{
+        content: { type: String },
+        senderId: { type: Schema.ObjectId },
+        chat_time: { type: Date }
+    }]
+
+})
+
+var PostSchema = Schema({
+    senderId: { type: Schema.ObjectId, required: true },
+    content: { type: String, required: true },
+    comments: [{
+        senderId: { type: Schema.ObjectId },
+        content: { type: String }
+    }]
+})
+
+var TransactionSchema = Schema({
+    good_id:{type:Schema.ObjectId,required:true},
+    seller_id:{type:Schema.ObjectId,required:true},
+    consumer_id:{type:Schema.ObjectId,required:true},
+    transaction_time:{type:Date}
+})
+
+UserModel = mongoose.model('User', UserSchema);
+GoodModel = mongoose.model('Good', GoodSchema);
+ChatModel = mongoose.model('Chat', ChatSchema);
+PostModel = mongoose.model('Post', PostSchema);
+TransactionModel = mongoose.model('Transaction', TransactionSchema);
+
+createUser = (name, password, email, school) => new Promise((resolve, reject) => {
+    UserModel.findOne({ email: email }, (err, user) => {
+        if (err) reject(err);
+        else if (!user) {
+            UserModel.create({ name: name, password: password, email: email, school: school }, (err, result) => {
+                if (err || !result) reject(err);
+                else {
+                    resolve(true);
+                }
             });
         }
-        else{
+        else {
             resolve(false);
         }
     });
- })
+})
 
- module.exports = {
-    createUser : createUser
-  };
+module.exports = {
+    createUser: createUser
+};
--- src/Nav.js
+++ src/Nav.js
@@ -1,26 +1,31 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-class Nav extends Component{
-    constructor(props){
+class Nav extends Component {
+    constructor(props) {
         super(props);
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <nav>
-	          <ul className="nav-links" >
-	            <Link to='/booklist'>
-	              <li>书单</li>
-		        </Link>
-                <Link to='/comment'>
-                    <li>采购留言</li>
-                </Link>
-                <Link to='/comment'>
-                    <li>商品展示</li>
-                </Link>
-              </ul>
+                <ul className="nav-links" >
+                    <Link to='/booklist'>
+                        <li>书单</li>
+                    </Link>
+                    <Link to='/comment'>
+                        <li>采购留言</li>
+                    </Link>
+                    <Link to='/comment'>
+                        <li>商品展示</li>
+                    </Link>
+                </ul>
+                <React.Fragment>
+                    <div className="user">{this.props.username}</div>
+                    <button 
+                    > Logout </button>
+                </React.Fragment>
             </nav>
         );
     }
