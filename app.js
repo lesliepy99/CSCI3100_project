21dab32653cbc57588c0d92e9b8bc3da14712fa5
lesliepy99 <1155124579@link.cuhk.edu.hk>
2021-03-17 21:23:29 +0800

    create_user

--- app.js
+++ app.js
@@ -7,4 +7,23 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/build/index.html')
 });
 
+
+app.post('/register',(req,res)=>{
+    console.log("Hello");
+    
+    console.log(req.body);
+    console.log("done");
+    db.createUser(name,password,email,school)
+       .then(
+           result=>{
+               if(result){
+                   res.status(200).send("Registered!");
+               }
+               else{
+                   res.status(403).send("Overlapped!");
+               }
+           },
+           err=>{res.status(500).send(err.toString())}
+       );   
+});
 app.listen(3000);
--- database.js
+++ database.js
@@ -13,3 +13,42 @@ db.on('error',console.error.bind(console,'connection:error:'));
 db.once('open',function(){
     console.log("Mongodb connected!");
 })
+
+var UserSchama = Schema({
+    name:{type:String, required:true},
+    password:{type:String, required:true},
+    email:{type:String,required:true,unique:true},
+    school:{type:String,required:true},
+    year:{type:Number},
+    ranki:{type:String},
+    location:{type:String},
+    chat_list:[{
+        userId:{type:Schema.ObjectId},
+        last_chat_time:{type:Date},
+        most_recent_message:{type:String},
+        unread_count:{type:Number}
+        }]
+});
+
+UserModel = mongoose.model('User',UserSchama);
+
+createUser = (name,password,email,school)=> new Promise((resolve,reject)=>{
+    UserModel.findOne({email:email},(err,user)=>{
+        if(err) reject(err);
+        else if(!user){
+            UserModel.create({name:name,password:password,email:email,school:school},(err,result)=>{
+               if (err || !result) reject(err);
+               else {
+                   resolve(true);
+               }
+            });
+        }
+        else{
+            resolve(false);
+        }
+    });
+ })
+
+ module.exports = {
+    createUser : createUser
+  };
