dece8bbdd063f98d39c7d33d197e3dc7ff071cfc
Hudson7777 <1155124383@link.cuhk.edu.hk>
2021-04-13 16:08:44 +0800

    Create CommentInput.js

---
+++ src/post_components/CommentInput.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+class CommentInput extends Component {
+  constructor () {
+    super()
+    this.state = {
+      content: ''
+    }
+  }
+
+
+  handleContentChange (event) {
+    this.setState({
+      content: event.target.value
+    })
+  }
+
+  handleSubmit () {
+    if (this.props.onSubmit) {
+      this.props.onSubmit({
+        content: this.state.content,
+      })
+    }
+    this.setState({ content: '' })
+  }
+
+  render () {
+    return (
+      <div className='comment-input'>
+        <div className='comment-field'>
+          <span className='comment-field-name'>Comment Content：</span>
+          <div className='comment-field-input'>
+            <textarea
+              value={this.state.content}
+              onChange={this.handleContentChange.bind(this)} />
+          </div>
+        </div>
+        <div className='comment-field-button'>
+          <button
+            onClick={this.handleSubmit.bind(this)}>
+            Comment
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CommentInput
