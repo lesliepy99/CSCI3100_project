791b45c7e12e506f10970f060a66d61563d9ce74
Hudson7777 <1155124383@link.cuhk.edu.hk>
2021-04-13 16:46:51 +0800

    Update CommentContent.js

--- src/post_components/CommentContent.js
+++ src/post_components/CommentContent.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
-import CommentInput from './CommentInput'
-import CommentList from './CommentList'
+import React, { Component } from 'react';
+import CommentInput from './CommentInput';
+import CommentList from './CommentList';
+import './comment_index.css'
 
 class CommentContent extends Component {
   constructor () {
@@ -10,20 +11,46 @@ class CommentContent extends Component {
     }
   }
 
+  componentWillMount () {
+    this._loadComments()
+  }
+
+  _loadComments () {
+    let comments = localStorage.getItem('comments')
+    if (comments) {
+      comments = JSON.parse(comments)
+      this.setState({ comments })
+    }
+  }
+
+  _saveComments (comments) {
+    localStorage.setItem('comments', JSON.stringify(comments))
+  }
+
   handleSubmitComment (comment) {
     if (!comment) return
-    if (!comment.content) return alert('Please type your comment!')
-    this.state.comments.push(comment)
-    this.setState({
-      comments: this.state.comments
-    })
+    if (!comment.username) return alert('Please Type Username')
+    if (!comment.content) return alert('Please Comment Here')
+    const comments = this.state.comments
+    comments.push(comment)
+    this.setState({ comments })
+    this._saveComments(comments)
+  }
+
+  handleDeleteComment (index) {
+    const comments = this.state.comments
+    comments.splice(index, 1)
+    this.setState({ comments })
+    this._saveComments(comments)
   }
 
   render() {
     return (
       <div className='wrapper'>
         <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
-        <CommentList comments={this.state.comments}/>
+        <CommentList
+          comments={this.state.comments}
+          onDeleteComment={this.handleDeleteComment.bind(this)} />
       </div>
     )
   }
