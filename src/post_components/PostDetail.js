b4a0854f6945c8f2b352ba047c68d11f71133f96
Hudson7777 <1155124383@link.cuhk.edu.hk>
2021-04-13 12:14:46 +0800

    Create PostDetail.js

---
+++ src/post_components/PostDetail.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import 'C:/Users/yanyu/Documents/GitHub/CSCI3100_project/src/App.css';
+import Button from '@material-ui/core/Button';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import IconButton from '@material-ui/core/IconButton';
+import Paper from '@material-ui/core/Paper';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import CreateIcon from '@material-ui/icons/Create';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import TextField from '@material-ui/core/TextField';
+import {Route, NavLink, Switch, Redirect, Link} from 'react-router-dom'
+
+
+export default function PostDetail() {
+    return (
+        <h1>This is the PostDetail Page</h1>
+
+    )
+}
