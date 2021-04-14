f9f0cc2ac81631e7f591c296d6ce56a4239d16f8
Peter <63060182+PeterShone@users.noreply.github.com>
2021-04-14 23:58:40 +0800

    Update Display.js

--- src/booklist_components/Display.js
+++ src/booklist_components/Display.js
@@ -15,6 +15,9 @@ import { Link } from 'react-router-dom';
 import IconButton from '@material-ui/core/IconButton';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Header from './Header';
+
+import getImageUrl from '../utils/getImageUrl';
 
 import { useState, useEffect } from 'react';
 
@@ -66,22 +69,27 @@ const Display = props => {
             }
             );
             console.log(myId);
+<<<<<<< Updated upstream
             alert('Good_id "' + goodId + '" has been added to your shopping cart');
         })();
+=======
+            
+        })().then(alert('Your ' + goodId + ' has been uploaded to platform '));
+>>>>>>> Stashed changes
 
     };
-    
+
     let len = products.length;
     //console.log(len);
     let list = [];
-    
+
     for (var i = 1; i < len; i++) {
-        if(i*9 >=len){
+        if (i * 9 >= len) {
             list.push(i);
             i = len;
         }
-        else{
-        list.push(i);
+        else {
+            list.push(i);
         }
     }
     //console.log(list);
@@ -89,12 +97,12 @@ const Display = props => {
     const [flag, setFlag] = React.useState('');
     const [goods, setGoods] = React.useState('');
 
-    const changePage = async(e, pageNumber) => {
+    const changePage = async (e, pageNumber) => {
         console.log(e, pageNumber);
 
         let array = products;
         setFlag(pageNumber);
-        setGoods(array.slice((pageNumber-1)*9, pageNumber*9));
+        setGoods(array.slice((pageNumber - 1) * 9, pageNumber * 9));
     };
     //console.log(flag);
     //console.log(goods);
@@ -104,90 +112,92 @@ const Display = props => {
     sample = sample.slice(0, 9);
 
 
-    if(goods.length != 0){
-    return (
-        <Container className={classes.cardGrid} maxWidth="md">
-            <div pclassName={classes.catagory}>
+    if (goods.length != 0) {
+        return (
+            <Container className={classes.cardGrid} maxWidth="md">
+                <div pclassName={classes.catagory}>
                 <Typography variant="h8" component="h2" paddingRight={140} gutterBottom>
                     {catagory}
                 </Typography>
             </div>
 
-            <Grid container spacing={4}>
-                {goods && goods.map((post) => {
-                    if (post) {
-                        return (
-                            <Grid item key={post} xs={12} sm={4} md={4}>
-                                <Card className={classes.card}>
-                                    <CardActionArea>
-                                        <CardMedia
-                                            className={classes.cardMedia}
-                                            image="https://source.unsplash.com/random"
-                                            title="Image title"
-                                        />
-                                    </CardActionArea>
+                <Grid container spacing={4}>
+                    {goods && goods.map((post) => {
+                        if (post) {
+                            return (
+                                <Grid item key={post} xs={12} sm={4} md={4}>
+                                    <Card className={classes.card}>
+                                        <CardActionArea>
+                                            <CardMedia
+                                                className={classes.cardMedia}
+                                                image= {getImageUrl("good_image", post._id)} //"https://source.unsplash.com/random"
+                                                title="Image title"
+                                            />
+                                        </CardActionArea>
 
-                                    <CardContent className={classes.cardContent}>
-                                        <Typography gutterBottom variant="h6" >
-                                            {post.name}
-                                        </Typography>
-                                        <Typography variant="h8" component="h2">
-                                            HK${post.estimated_price}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions >
-                                        <Link to={{
-                                            pathname: `/product/${post._id}`, state: {
-                                                id: post._id, name: post.name, price: post.estimated_price,
-                                                description: post.description, sellerId: post.userId, myId: props.myId
-                                            }
-                                        }} style={{ textDecoration: 'none' }} className="nav-link">
-                                            <Button variant="contained" size="small" color="secondary" disableElevation>
-                                                detail
+                                        <CardContent className={classes.cardContent}>
+                                            <Typography gutterBottom variant="h6" >
+                                                {post.name}
+                                            </Typography>
+                                            <Typography variant="h8" component="h2">
+                                                HK${post.estimated_price}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions >
+                                            <Link to={{
+                                                pathname: `/product/${post._id}`, state: {
+                                                    id: post._id, name: post.name, price: post.estimated_price, tags: post.tags,
+                                                    description: post.description, sellerId: post.userId, myId: props.myId, allUser: props.allUser
+                                                }
+                                            }} style={{ textDecoration: 'none' }} className="nav-link">
+                                                <Button variant="contained" size="small" color="secondary" disableElevation>
+                                                    detail
                                             </Button>
-                                        </Link>
-                                        <IconButton color="secondary" aria-label="add to shopping cart" onClick={(e) => addCart(e, post._id)}>
-                                            <AddShoppingCartIcon />
-                                        </IconButton>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        )
-                    }
-                    return null
-                })}
-            </Grid>
-            
-            <div style={{paddingTop: 16, align: "center"}} align="center">
-            <ButtonGroup color="secondary" aria-label="outlined secondary button group">
-                {list.map((page) => {
-                    if(flag && flag==page){
-                    return(    
-                    <Button variant="contained" onClick={(e) => changePage(e, page)}>
-                        {page}
-                    </Button>
-                    )}
-                    else{
-                        return(    
-                            <Button  onClick={(e) => changePage(e, page)}>
-                                {page}
-                            </Button>
-                        )
-                    }
-                })}
-                </ButtonGroup>
+                                            </Link>
+                                            <IconButton color="secondary" aria-label="add to shopping cart" onClick={(e) => addCart(e, post._id)}>
+                                                <AddShoppingCartIcon />
+                                            </IconButton>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            )
+                        }
+                        return null
+                    })}
+                </Grid>
+
+                <div style={{ paddingTop: 16, align: "center" }} align="center">
+                    <ButtonGroup color="secondary" aria-label="outlined secondary button group">
+                        {list.map((page) => {
+                            if (flag && flag == page) {
+                                return (
+                                    <Button variant="contained" onClick={(e) => changePage(e, page)}>
+                                        {page}
+                                    </Button>
+                                )
+                            }
+                            else {
+                                return (
+                                    <Button onClick={(e) => changePage(e, page)}>
+                                        {page}
+                                    </Button>
+                                )
+                            }
+                        })}
+                    </ButtonGroup>
                 </div>
-        </Container>
-    );}
-    else{
+            </Container>
+        );
+    }
+    else {
         return (
             <Container className={classes.cardGrid} maxWidth="md">
-                <div className={classes.catagory}>
-                    <Typography variant="h8" component="h2" paddingRight={140} gutterBottom>
-                        {catagory}
-                    </Typography>
+                <div pclassName={classes.catagory}>
+                <Typography variant="h8" component="h2" paddingRight={140} gutterBottom>
+                    {catagory}
+                </Typography>
                 </div>
-    
+
                 <Grid container spacing={4}>
                     {sample.map((post) => {
                         if (post) {
@@ -201,7 +211,7 @@ const Display = props => {
                                                 title="Image title"
                                             />
                                         </CardActionArea>
-    
+
                                         <CardContent className={classes.cardContent}>
                                             <Typography gutterBottom variant="h6" >
                                                 {post.name}
@@ -213,8 +223,8 @@ const Display = props => {
                                         <CardActions >
                                             <Link to={{
                                                 pathname: `/product/${post._id}`, state: {
-                                                    id: post._id, name: post.name, price: post.estimated_price,
-                                                    description: post.description, sellerId: post.userId, myId: props.myId
+                                                    id: post._id, name: post.name, price: post.estimated_price, tags: post.tags,
+                                                    description: post.description, sellerId: post.userId, myId: props.myId, allUser: props.allUser
                                                 }
                                             }} style={{ textDecoration: 'none' }} className="nav-link">
                                                 <Button variant="contained" size="small" color="secondary" disableElevation>
@@ -232,26 +242,27 @@ const Display = props => {
                         return null
                     })}
                 </Grid>
-                
-                <div style={{paddingTop: 16, align: "center"}} align="center">
-                <ButtonGroup color="secondary" aria-label="outlined secondary button group">
-                    {list.map((page) => {
-                        if((flag && flag==page) || (!flag && page==1)){
-                        return(    
-                        <Button variant="contained" onClick={(e) => changePage(e, page)}>
-                            {page}
-                        </Button>
-                        )}
-                        else{
-                            return(    
-                                <Button  onClick={(e) => changePage(e, page)}>
-                                    {page}
-                                </Button>
-                            )
-                        }
-                    })}
+
+                <div style={{ paddingTop: 16, align: "center" }} align="center">
+                    <ButtonGroup color="secondary" aria-label="outlined secondary button group">
+                        {list.map((page) => {
+                            if ((flag && flag == page) || (!flag && page == 1)) {
+                                return (
+                                    <Button variant="contained" onClick={(e) => changePage(e, page)}>
+                                        {page}
+                                    </Button>
+                                )
+                            }
+                            else {
+                                return (
+                                    <Button onClick={(e) => changePage(e, page)}>
+                                        {page}
+                                    </Button>
+                                )
+                            }
+                        })}
                     </ButtonGroup>
-                    </div>
+                </div>
             </Container>
         );
     }
